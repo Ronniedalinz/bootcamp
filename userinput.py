4dd531109362ee2c32d5324814b472f6e7fbfa99
Ronniedalinz <ochiengronald95@gmail.com>
2021-07-15 22:32:25 +0300

    Create userinput.py

---
+++ userinput.py
@@ -0,0 +1,11 @@
+num =int(input('Please Enter Your Mark:\n'))
+if num <=100 and num>=90:
+    print ('You are geek and You got A grade !')
+elif num<=89 and num>=80:
+    print('You are a bookworm and you got Grade B!')
+elif num<=79 and num>=70:
+    print('You perfomed averagely and got Grade C!')
+elif num<=69 and num>=60:
+    print('You did your best and got Grade D!') 
+else:
+    print('You failed and got a retake, Pull up!')
