fc6a97d29b33f2137afe6394dca92a76d7c61e65
Ronniedalinz <ochiengronald95@gmail.com>
2021-07-28 18:52:14 +0300

    Create function.py

---
+++ function.py
@@ -0,0 +1,11 @@
+# A simple python function to check
+# whether x is even or odd
+
+def evenodd(x):
+    if (x % 2 ==0):
+        print ("even")
+    else:
+        print ("odd")
+# Driver code to call the fuction
+evenodd(2)
+evenodd(3)        
